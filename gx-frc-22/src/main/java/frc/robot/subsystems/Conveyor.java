961c822d05ba87600b648aff87c04cc9d210c35d
esenecal <55216700+esenecal@users.noreply.github.com>
2022-02-01 21:06:54 -0800

    Added motors, speed to subsystem

--- gx-frc-22/src/main/java/frc/robot/subsystems/Conveyor.java
+++ gx-frc-22/src/main/java/frc/robot/subsystems/Conveyor.java
@@ -5,18 +5,47 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Conveyor extends SubsystemBase {
-  /** Creates a new Conveyor. */
-  public Conveyor() {}  
+  
+  // New motor controllers for motors.
 
-  private static CANSparkMax leftConveyor;
-  private static CANSparkMax rightConveyor;
+  // Make sure motor type is brushed or not.
+  private static CANSparkMax leftConveyorLead = new CANSparkMax(Constants.LEFT_CONVEYOR_PORT, MotorType.kBrushless);
+  private static CANSparkMax rightConveyorFollow = new CANSparkMax(Constants.RIGHT_CONVEYOR_PORT, MotorType.kBrushless);
+
+  // Create new instance for singleton
+  private static Conveyor instance;
+
+  /** Creates a new Conveyor. */
+  public Conveyor() {
+
+    // Right motor follows left. Invert right motor.
+    rightConveyorFollow.follow(leftConveyorLead);
+    rightConveyorFollow.setInverted(true);
+
+  }  
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  // Set the speed.
+  public void setMotorSpeed(double speed) {
+    leftConveyorLead.set(speed);
+  }
+
+  //Singleton to make sure there is only one instance of Conveyor.
+  public static Conveyor getInstance() {
+    if (instance == null) {
+      instance = new Conveyor();
+    }
+    
+    return instance;
   }
 }
