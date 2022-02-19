5326538fa8ddb8330b1f24a82fa880a9650ea1c4
esenecal <55216700+esenecal@users.noreply.github.com>
2022-02-18 17:05:49 -0800

    Everybot22 intake arm (#4)
    * hopefully this doesnt break anything
    
    * Update Ball_Intake.java
    
    * Ball intake is done
    
    * test
    
    * done
    
    * comments almost done
    
    haimish
    
    * PID
    
    We added the PID code from the video.
    We made a new subsystem, it is basically replacing the command.
    
    * Fixing intakeArm subsystem
    
    removed unessacary code and finished code from east robotics tutorial. subsystem and command (80% done)
    
    * changed commands
    
    yes
    
    * editing for everybot
    
    removed code that is made obsolete by everybot and also made intake commands work
    
    * a
    
    * Update IntakeArm.java
    
    formatting fixes and creating singular motor
    
    * testing
    
    this is for everybot ball intake
    
    * commands done
    
    ball intake
    
    * Ball Intake and Arm done except arm command
    
    arm command is needed, everything else is done, need to change branch name to EB-arm-and-intake or something.
    
    Co-authored-by: ShrishPremkrishna <shrish.premkrishna@gmail.com>
    Co-authored-by: LittleTurtleJ <justinb84128@gmail.com>
    Co-authored-by: Haimish1 <yesgreen674@gmail.com>

---
+++ gx-frc-22/src/main/java/frc/robot/subsystems/BallIntake.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+//import com.revrobotics.CANEncoder;
+// Person of unkown identity(***redacted***) had name idea
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+  /** Creates a new Ball_Intake. */
+public class BallIntake extends SubsystemBase {
+static private CANSparkMax leader = new CANSparkMax(5, MotorType.kBrushless);
+
+static BallIntake intake;
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+
+  }
+
+
+  //Starts sucking in balls
+  public void ballIn()
+  {
+    leader.set(0.7);
+  }
+
+
+  //Starts shooting out balls
+  public void ballOut()
+  {
+    leader.set(-0.7);
+  }
+
+  // Singleton - makes only one instance of BallIntake
+  public static BallIntake getInstance(){
+    if(intake == null)
+    {
+      intake = new BallIntake();
+    }
+
+    return intake;
+  }
+
+  
+
+  }
+
